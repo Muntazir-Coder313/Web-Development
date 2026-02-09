09d078092e937ee90fc13bc07a14a825471f4441
Muntazir-Coder <muntazirma21@gmail.com>
2026-02-08 19:24:10 -0800

    This is a task in which  i use conditional statement to make a grading system from A++ to Fail this is very interesting task

---
+++ Web Development/Java Script/Tasks/Conditional-Grade.js
@@ -0,0 +1,21 @@
+let grade="Grade";
+let marks= 100;
+if(marks>=95 && marks<=100){
+    console.log("A++");
+}
+else if(marks>=90 && marks<=95){
+    console.log("A+");
+}
+else if(marks>=80 && marks<=90){
+    console.log("B");
+}
+else if(marks>=70 && marks<=80){
+    console.log("C");
+}
+else if(marks>=60 && marks<=70){
+    console.log("D");
+}
+else{
+    console.log("Fail");
+    
+}
