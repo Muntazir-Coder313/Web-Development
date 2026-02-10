4d472eefacf510a910b71b722393f367ebc12488
Muntazir-Coder <muntazirma21@gmail.com>
2026-02-09 16:05:03 -0800

    This is a task in which  i use conditional statement to make a grading system from A++ to Fail this is very interesting task

---
+++ Web Development/Java Script/JS Lecture/Switch-Property.js
@@ -0,0 +1,15 @@
+let operator= "*";
+switch (operator) {
+    case "-":
+        console.log("Sub");
+        break;
+    case "+":
+        console.log("Sum");
+        break;
+     case "*":
+        console.log("Mul");
+        break;
+    default:
+        console.log("Invalid Operator");
+        break;
+}
