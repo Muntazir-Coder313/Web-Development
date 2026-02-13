84fe0efd5f4d58a281e16c870650573613b384a9
Muntazir-Coder <muntazirma21@gmail.com>
2026-02-13 11:35:17 -0800

    This a multiplication table and a star pattern of shapes

--- Web Development/Java Script/JS Lecture/Do-While-Loop.js
+++ Web Development/Java Script/JS Lecture/Do-While-Loop.js
@@ -18,4 +18,4 @@ do{
     }
   j++
     
-}while(j<10)
+}while(j<=10)
---
+++ Web Development/Java Script/JS Lecture/Nested-Loop.js
@@ -0,0 +1,39 @@
+// Nested Loop of Multiplication Table
+// console.log("======================Multiplication Table======================");
+// for(let i=1;i<=10;i++){
+//     for(let j=1;j<=10;j++){
+//         console.log(`${i} x ${j} = ${i*j}`);
+//     }
+// }
+
+
+
+// Nested Loop of Star Pattern
+// Triangle Star Pattern
+// console.log("======================Triangle Star Pattern======================");
+
+// for(let i=1;i<=5;i++){
+//     let str = '';
+//     for(let j=1;j<=i;j++){
+//         str += '*';
+//     }
+//     console.log(str);
+// }
+
+
+// Square Star Pattern
+console.log("======================Square Star Pattern======================");
+
+
+for(let i=4;i>=1;i--){
+    let str='';
+    for(let j=4;j>=1;j--){
+        str +='*'
+    }
+    console.log(str);
+    
+}
+
+
+
+
