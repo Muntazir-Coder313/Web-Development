0828ac42e23bc4ad04bd4619a6ecdd7a07af490b
Muntazir-Coder <muntazirma21@gmail.com>
2026-02-13 19:35:15 -0800

    This is Function and as well as it is lecture

---
+++ Web Development/Java Script/JS Lecture/Function.js
@@ -0,0 +1,38 @@
+// Fuction is a block of code which perform a specific task and it is reusable also. It is also called as method in java script. It is defined with the function keyword followed by the name of the function and parentheses().
+
+
+
+
+// Function Addition
+function add(a,b){
+   console.log(a+b);
+   
+}
+add(20,50)
+
+
+
+// Function Subtraction
+function sub(a,b){
+   return a-b;
+}
+let result=sub(60,20);
+console.log(result);
+
+
+
+// Function Multiplication
+function mul(a,b){
+   return a*b;
+}
+let result1=mul(20,20);
+console.log(result1);
+
+
+
+// Function Division
+function div(a,b){
+   return a/b;
+}
+let result2=div(100,20);
+console.log(result2);
