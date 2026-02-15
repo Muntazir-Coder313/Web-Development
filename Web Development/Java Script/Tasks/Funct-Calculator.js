425c3d09cdd5b2ebb8baa4edd9b6fbb9e943aa21
Muntazir-Coder <muntazirma21@gmail.com>
2026-02-14 18:48:35 -0800

    Add a Calculator Task

---
+++ Web Development/Java Script/Tasks/Funct-Calculator.js
@@ -0,0 +1,48 @@
+
+
+console.log("<<<<<<<<<<================== PROJECT ==================>>>>>>>>>>");
+console.log("<<<<<<<================== CALCULATOR ==================>>>>>>>>>>");
+console.log("<<<<<<<----Made by : Muntazir---->>>>>>");
+
+// // // Add
+// function sum(a,b){
+//     return a+b;
+    
+// }
+// let result4="Adition : "+sum(20,90)
+// console.log(result4);
+
+// // // Sub
+// function sub(a,b){
+//     return a-b;
+// }
+// let result1="Subtraction : "+sub(90,20)
+// console.log(result1);
+
+// // // Mul
+// function mul(a,b){
+//     return a*b;
+// }
+// let result2="Multiplication : "+mul(20,90)
+// console.log(result2);
+
+// // // Div
+// function div(a,b){
+//     return a/b;
+// }
+// let result3="Division : "+div(200,10)
+// console.log(result3);
+
+
+// Function Calculator
+console.log("========== Function Calculator ==========");
+
+function Calculator(a,b){
+    console.log("Adition : "+(a+b));
+    console.log("Division : "+(a/b));
+    console.log("Multiplication : "+(a*b));
+    console.log("Subtraction : "+(a-b));
+}
+let result=Calculator(200,10)
+
+
