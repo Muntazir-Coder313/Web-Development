f8dd69d3b62255db311074fc79447f24ff08aa01
Muntazir-Coder <muntazirma21@gmail.com>
2026-02-16 18:40:24 -0800

    Add a arrow ternary operator it make our ork easy and it is very helpful

---
+++ Web Development/Java Script/JS Lecture/Arrow-Ternary.js
@@ -0,0 +1,10 @@
+// // Vote Machine ?
+// let votemachine=(age)=> age>=18? "Can vote" : "Can't Vote"
+// console.log(votemachine(20));
+// console.log(votemachine(25));
+
+
+//Tax ?
+let driver=(tax)=> tax=1000? "You may take and drive car" : "Can't take car 1st shoud pay tax"
+console.log(driver(2000));
+console.log(driver(2500));
