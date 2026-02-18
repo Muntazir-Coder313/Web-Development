b428385d403f574adbc9808cd4f299f2e5dc20fa
Muntazir-Coder <muntazirma21@gmail.com>
2026-02-18 10:21:43 -0800

    Higher-Order-Function add a file

---
+++ Web Development/Java Script/JS Lecture/Higher-Order-Function.js
@@ -0,0 +1,50 @@
+// 
+// function add(a){
+//   return a+2;
+// }
+// function mul(a){
+//   return a*3;
+// }
+// function compose(f ,g){
+//   return function(a){
+//     return f(g(a));
+//   }
+// }
+// var result=compose(add,mul)(4);
+// console.log(result);
+
+
+
+// Currying
+// function mul(a){
+//   return function(b){
+//     return a*b
+//   }
+// }
+// var result=mul(9);
+// console.log(result(7));
+
+
+
+
+// Memorization
+function memoize(func) {
+    var cache = {};
+    return function (arg) {
+        if (cache[arg]) {
+            return cache[arg];
+        } else {
+            var res = func(arg);
+            cache[arg] = res;
+            return res;
+        }
+    };
+}
+function slow(num) {
+    console.log("Computing...");
+    return num * 2;
+}
+
+var fast = memoize(slow);
+console.log(fast(5)); // Computing... 10
+console.log(fast(5)); // 10 (cached)
