ca9052033108d5c4baa82688aea276b448d171c1
Muntazir-Coder <muntazirma21@gmail.com>
2026-02-19 17:33:40 -0800

    This is Array Function Add this lecture

---
+++ Web Development/Java Script/JS Lecture/Array.js
@@ -0,0 +1,36 @@
+// Array
+let a=[1,2,3,4,5];
+console.log(a);
+
+
+
+// Indexing
+console.log(a[0]);
+console.log(a[1]);
+console.log(a[2]);
+console.log(a[3]);
+console.log(a[4]);
+console.log(a);
+
+
+// Push
+a.push(6);
+a.push(7);
+
+
+// Unshift
+a.unshift(0);
+console.log(a);
+
+
+// Pop
+a.pop();
+a.pop();
+a.pop();
+console.log(a);
+
+
+// Shift
+a.shift();
+console.log(a);
+
