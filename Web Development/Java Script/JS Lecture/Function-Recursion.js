b78ff3d1f485741e62bf5d42a03d1f8d03fb543c
Muntazir-Coder <muntazirma21@gmail.com>
2026-04-04 17:22:29 -0800

    This is function recursion add it

--- Web Development/Java Script/JS Lecture/Function-Recursion.js
+++ Web Development/Java Script/JS Lecture/Function-Recursion.js
@@ -1,1 +1,24 @@
-// n x f-1
+// let num = 5;
+// let result = 1;
+// let expression = "";
+
+// for (let i = 1; i <= num; i++) {
+//     result *= i;
+//     expression += i;
+
+//     if (i < num) {
+//         expression += " * ";
+//     }
+// }
+
+// console.log(num + "! = " + expression);
+// console.log("Answer = " + result);
+
+function factorial(n){
+    if(n===1){
+        return 1;
+    }
+    return n*factorial(n-1);
+}
+console.log(factorial(3));
+
