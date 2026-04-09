638105f5694cfb043345a4e35e72b33082b23d93
Muntazir-Coder <muntazirma21@gmail.com>
2026-04-08 17:01:26 -0800

    This is Promis add it

--- Web Development/Java Script/JS Lecture/Promises.js
+++ Web Development/Java Script/JS Lecture/Promises.js
@@ -12,18 +12,50 @@
 // .then(data=>console.log(data))
 // .catch(err=>console.log(err));
 
-function login(username,password){
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            if(username==="admin"&&password==="1234"){
-                resolve("Login successful!");
-            }
-            else{
-                reject("Invalid Username or Password");
-            }
-        }, 1000);
-    })
-}
-login("admin","1234")
-.then(message=>console.log(message))
-.catch(error=>console.error(error));
+// function login(username,password){
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             if(username==="admin"&&password==="1234"){
+//                 resolve("Login successful!");
+//             }
+//             else{
+//                 reject("Invalid Username or Password");
+//             }
+//         }, 1000);
+//     })
+// }
+// login("admin","1234")
+// .then(message=>console.log(message))
+// .catch(error=>console.error(error));
+
+
+// let objpromis=new Promise((resolve, reject) => {
+//     let obj={
+//         name:"Muntazir",
+//         age:"21",
+//         email:"muntu@gmail.com"
+//     }
+//     resolve(obj);
+//     reject("Error fetching data");
+// });
+// objpromis
+// .then(data=>console.log(data))
+// .then(data=>console.log("Data fetched successfully!"))
+// .catch(error=>console.log(error))
+
+
+const fetchData=new Promise((resolve, reject) => {
+    setTimeout(() => {
+        const data={
+            name:"MA",
+            age:"21",
+            email:"muntuMA@gmail.com"
+        }
+        resolve(data);
+    }, 2000);
+});
+
+fetchData
+.then(data=>console.log(data.name))
+.then(data=>console.log("Data fetched successfully!"))
+.catch(error=>console.log(error))
